8c1096a4dc510ee6a6f04922e979e8a59cd41636
Marc von Glahn <marc.von.glahn@gmail.com>
2023-11-04 12:05:36 +0100

    [feat]: Added AudioManager and Example background music

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Custom Playback Options")]
+    [SerializeField] bool playRandom = true;
+    [SerializeField] int backgroundClipRandomIndex = 0;
+
+    [Header("Audio Source")]
+    [SerializeField] AudioSource backgroundSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("Audio Clips")]
+    [SerializeField] List<AudioClip> backgroundAudioClips;
+
+
+    private void Start()
+    {
+        if(!playRandom)
+        {
+            PlayBackgroundClip(backgroundClipRandomIndex);
+            return;
+        }
+
+        PlayBackgroundClip(Random.Range(0, backgroundAudioClips.Count));
+    }
+
+
+    #region Background
+    //Assigns the clip at index to backgroundSource and starts playing it.
+    private void PlayBackgroundClip(int index)
+    {
+        backgroundSource.clip = backgroundAudioClips[index];
+
+        backgroundSource.Play();
+    }
+    #endregion
+
+
+    #region SFX
+    public void PlaySFX(AudioClip audioClip)
+    {
+        sfxSource.PlayOneShot(audioClip);
+    }
+
+    #endregion
+}
