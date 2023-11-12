932aeb997cc8ba721724f5bfb3ccd0074fb3def4
alexw-su <alex.w.su@hotmail.com>
2023-11-12 12:08:50 +0100

    [feat]: gradually increases distortion instead of instant distortion

--- Assets/Scripts/PaintingScript.cs
+++ Assets/Scripts/PaintingScript.cs
@@ -10,12 +10,30 @@ public class PaintingScript : MonoBehaviour, IInteractable
     public Material distortionMaterial;
 
     private PlayerController _playerController;
+
+    // For Fade Transition
     private float transitionTime;
+
+    // For Distortion
+    private float blend;
+    private float lerpDuration; 
+    private float minDistortionValue; 
+    private float maxDistortionValue; 
+    private float timeElapsed;
 
     void Start()
     {
-        transitionTime = 1f;
         _playerController = FindObjectOfType<PlayerController>();
+        
+        // For Fade Transition
+        transitionTime = 1f;
+        
+        // For Distortion
+        blend = 0;
+        lerpDuration = 1;
+        minDistortionValue = 0; 
+        maxDistortionValue = 1; 
+        timeElapsed = 0;
     }
 
     public void OnInteractionStart(bool isGrabbing)
@@ -24,23 +42,45 @@ public class PaintingScript : MonoBehaviour, IInteractable
             return;
         
         StartCoroutine(WindUpTeleport());
+        StartCoroutine(GradualDistortion());
 
         //SendMessagesToMessageBus();
     }
 
+    // Coroutine that teleports player after the fade out transition finishes.
     private IEnumerator WindUpTeleport()
     {
-        // Play Fade transition
+        // Play fade out transition
         transition.SetTrigger("FadeOut");
 
-        // Wait for Fade
-        distortionMaterial.SetFloat("_Blend", 0.5f);
+        // Wait for fade out
         yield return new WaitForSeconds(transitionTime);
 
         // Teleport Player
         Teleport();
     }
 
+    // Coroutine that gradually increases the distortion.
+    private IEnumerator GradualDistortion()
+    {     
+        while (timeElapsed < 1)
+        {
+            // Sets distortion amount based on time elapsed
+            blend = Mathf.Lerp(minDistortionValue, maxDistortionValue, timeElapsed / lerpDuration);
+
+            // Updates time elapsed
+            timeElapsed += Time.deltaTime;
+
+            // Sets distortion amount to the distortion material (to distort screen)
+            distortionMaterial.SetFloat("_Blend", blend);
+            yield return null;
+        }
+
+        // When time elapsed is above 1, set distortion to max.
+        blend = maxDistortionValue;
+    }
+
+    // Teleports player to specified transform location
     private void Teleport()
     {
         if (_playerController != null)
@@ -52,7 +92,13 @@ public class PaintingScript : MonoBehaviour, IInteractable
                 messageGeneral.SendMessageByKey(messageKey);
             }
         }
+
+        // Reset Distortion
+        blend = 0f;
+        timeElapsed = 0;
         distortionMaterial.SetFloat("_Blend", 0f);
+        
+        // Fade in
         transition.SetTrigger("FadeIn");
     }
 
