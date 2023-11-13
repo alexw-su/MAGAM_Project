054d8fdc371a5f45572a162956cc5ef924fd55b9
alexw-su <alex.w.su@hotmail.com>
2023-11-13 20:37:14 +0100

    [fixed]: Painting crash, added new script for puzzle1

--- Assets/Scripts/Puzzle1/InteractionStateChange.cs
+++ Assets/Scripts/Puzzle1/InteractionStateChange.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractionStateChange : MonoBehaviour
+public class InteractionStateChange : MonoBehaviour, IInteractable
 {   
     public Puzzle1 stateMachine;
     public puzzle1States state;
-    public GameObject gameObjectToCollide;
 
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject == gameObjectToCollide)
-        {
-            stateMachine.Change(state);
-        }
+    public void OnInteractionStart(bool isGrabbing) 
+    { 
+        if(isGrabbing)
+            return;
+
+        stateMachine.Change(state);
     }
 }
