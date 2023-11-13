c46dcc271145afdd38cc32fd577072173dc82c58
Alex Su <74763673+alexw-su@users.noreply.github.com>
2023-11-13 18:44:50 +0100

    [fix & feat]: Pixelated quick fix, VFX Manager added (#25)

--- Assets/Scripts/PaintingScript.cs
+++ Assets/Scripts/PaintingScript.cs
@@ -7,35 +7,16 @@ public class PaintingScript : MonoBehaviour, IInteractable
 {
     public GameObject puzzleLocation;
     public string messageKey;
-    public Animator transition;
-    public Material distortionMaterial;
+    
 
     private PlayerController _playerController;
-
-    // For Fade Transition
-    private float transitionTime;
-
-    // For Distortion
-    private float blend;
-    private float lerpDuration; 
-    private float minDistortionValue; 
-    private float maxDistortionValue; 
-    private float timeElapsed;
-
+    private VFXManager vfx;
 
     void Start()
     {
         _playerController = FindObjectOfType<PlayerController>();
-        
-        // For Fade Transition
-        transitionTime = 1f;
-        
-        // For Distortion
-        blend = 0;
-        lerpDuration = 1;
-        minDistortionValue = 0; 
-        maxDistortionValue = 1; 
-        timeElapsed = 0;
+        vfx = VFXManager.Instance;
+        print(vfx);
     }
 
     public void OnInteractionStart(bool isGrabbing)
@@ -43,8 +24,10 @@ public class PaintingScript : MonoBehaviour, IInteractable
         if (isGrabbing)
             return;
         
+        // Starts vfx transition
+        vfx.EnableFullScreenPassRendererFeature();
         StartCoroutine(WindUpTeleport());
-        StartCoroutine(GradualDistortion());
+        StartCoroutine(vfx.GradualIncreaseDistortion());
 
         //SendMessagesToMessageBus();
     }
@@ -52,34 +35,14 @@ public class PaintingScript : MonoBehaviour, IInteractable
     // Coroutine that teleports player after the fade out transition finishes.
     private IEnumerator WindUpTeleport()
     {
-        // Play fade out transition
-        transition.SetTrigger("FadeOut");
+        // Start fade transition
+        vfx.TriggerFadeOut();
 
-        // Wait for fade out
-        yield return new WaitForSeconds(transitionTime);
+        // Wait for fade
+        yield return new WaitForSeconds(vfx.transitionTime);
 
         // Teleport Player
         Teleport();
-    }
-
-    // Coroutine that gradually increases the distortion.
-    private IEnumerator GradualDistortion()
-    {     
-        while (timeElapsed < 1)
-        {
-            // Sets distortion amount based on time elapsed
-            blend = Mathf.Lerp(minDistortionValue, maxDistortionValue, timeElapsed / lerpDuration);
-
-            // Updates time elapsed
-            timeElapsed += Time.deltaTime;
-
-            // Sets distortion amount to the distortion material (to distort screen)
-            distortionMaterial.SetFloat("_Blend", blend);
-            yield return null;
-        }
-
-        // When time elapsed is above 1, set distortion to max.
-        blend = maxDistortionValue;
     }
 
     // Teleports player to specified transform location
@@ -101,12 +64,7 @@ public class PaintingScript : MonoBehaviour, IInteractable
             messageController.SendMessageByKey(messageKey);
         }
 
-        // Reset Distortion
-        blend = 0f;
-        timeElapsed = 0;
-        distortionMaterial.SetFloat("_Blend", 0f);
-        
-        // Fade in
-        transition.SetTrigger("FadeIn");
+        vfx.TriggerFadeIn();
+        StartCoroutine(vfx.GradualDecreaseDistortion());
     }
 }
