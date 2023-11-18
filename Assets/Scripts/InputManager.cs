3157a5b0566ea030f7394dfdbf66176ab57604f3
mwpodgorni <31960377+mwpodgorni@users.noreply.github.com>
2023-11-18 20:13:44 +0100

    [refactor] refactoring message system (#35)

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -40,7 +40,18 @@ public class InputManager : MonoBehaviour
     {
         playerInput.Enable();
     }
+    public void ToggleLookInput(bool toggle)
+    {
+        if (toggle)
+        {
+            playerInput.Player.Look.Enable();
+        }
+        else
+        {
+            playerInput.Player.Look.Disable();
+        }
 
+    }
     public Vector2 GetPlayerMovement()
     {
         return playerInput.Player.Movement.ReadValue<Vector2>();
@@ -88,7 +99,14 @@ public class InputManager : MonoBehaviour
     {
         return playerInput.Player.Sprint.IsPressed();
     }
-
+    public bool CKeyPressed()
+    {
+        return playerInput.Player.CPressed.triggered;
+    }
+    public bool YKeyPressed()
+    {
+        return playerInput.Player.YPressed.triggered;
+    }
     public bool GetPlayerQuit()
     {
         return playerInput.Player.Quit.triggered;
