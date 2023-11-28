3825c22eb799422af88df63656fb39736ccb7c09
mwpodgorni <31960377+mwpodgorni@users.noreply.github.com>
2023-11-28 15:33:00 +0100

    [refactor] new clock model (#46)

--- Assets/Scripts/Puzzle2/Puzzle2.cs
+++ Assets/Scripts/Puzzle2/Puzzle2.cs
@@ -14,12 +14,10 @@ public partial class Puzzle2 : MarcimanStateMachine
     private Puzzle2States _currentState;
 
     [Header("PuzzleElements")]
-    [SerializeField] SnapToLocation_Handler pointerSnapHandler;
     [SerializeField] PuzzleLeave puzzle2Leave;
     public GameObject clock1;
     public GameObject clock2;
     public GameObject crystal;
-    public GameObject doorBlock;
     [Space]
     public Material skyBoxMaterialNight;
 
@@ -41,13 +39,11 @@ public partial class Puzzle2 : MarcimanStateMachine
     }
     private void OnEnable()
     {
-        pointerSnapHandler.OnSnappedToLocation += PointerSnapHandler_OnSnappedToLocation;
         puzzle2Leave.OnLeavePainting += PaintingLeaveHandler;
     }
 
     private void OnDisable()
     {
-        pointerSnapHandler.OnSnappedToLocation -= PointerSnapHandler_OnSnappedToLocation;
         puzzle2Leave.OnLeavePainting += PaintingLeaveHandler;
     }
     private void ChangeState(Puzzle2States newState)
@@ -81,6 +77,7 @@ public partial class Puzzle2 : MarcimanStateMachine
             clock2.tag = "Untagged";
             if (messageSolved != null && messageBus != null)
             {
+                Debug.Log("messageSolved"+messageSolved);
                 messageBus.AddMessage(messageSolved.Category, messageSolved.Key);
                 messageSolved = null;
             }
