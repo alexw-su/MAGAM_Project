e8c978f505739f74539016c571c868dabcb96f7b
Marc von Glahn <57836704+MarcVonGlahn@users.noreply.github.com>
2023-12-05 11:43:20 +0100

    [feat]Puzzle4 second iteration (#65)

---
+++ Assets/Scripts/Puzzle4_StateMachine/Puzzle4_OnEnter_PlayElevator.cs
@@ -0,0 +1,40 @@
+using MoreMountains.Feedbacks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle4_OnEnter_PlayElevator : MonoBehaviour
+{
+    [SerializeField] MMF_Player door_ClosePlayer;
+    [SerializeField] float timeOffset = 2f;
+
+    private MMF_Player _elevatorPlayer;
+    private OnPlayerEnter_Trigger _onPlayerEnterTrigger;
+
+    void Start()
+    {
+        _elevatorPlayer = GetComponent<MMF_Player>();
+
+        _onPlayerEnterTrigger = GetComponent<OnPlayerEnter_Trigger>();
+        _onPlayerEnterTrigger.OnPlayerEnterTrigger += OnPlayerEnterTrigger_OnPlayerEnterTrigger;
+    }
+
+    private void OnDestroy()
+    {
+        _onPlayerEnterTrigger.OnPlayerEnterTrigger -= OnPlayerEnterTrigger_OnPlayerEnterTrigger;
+    }
+
+    private void OnPlayerEnterTrigger_OnPlayerEnterTrigger()
+    {
+        StartCoroutine(Elevator_Routine());
+    }
+
+    private IEnumerator Elevator_Routine()
+    {
+        door_ClosePlayer.PlayFeedbacks();
+
+        yield return new WaitForSeconds(timeOffset);
+
+        _elevatorPlayer.PlayFeedbacks();
+    }
+}
