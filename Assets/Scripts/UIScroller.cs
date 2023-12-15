971308e528f18f6d84f6d14a83bebb6059c0000e
Marc von Glahn <marc.von.glahn@gmail.com>
2023-12-15 15:52:45 +0100

    UIScroller

---
+++ Assets/Scripts/UIScroller.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScroller : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+
+    public float duration = 10.0f;
+
+    private float _timer;
+}
