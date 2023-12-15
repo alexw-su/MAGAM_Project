f2ba0a0658ec7bfc2af0e469cc8693608606a046
Marc von Glahn <marc.von.glahn@gmail.com>
2023-12-15 11:48:06 +0100

    [carry from other branch]: Some file changes

--- Assets/Scripts/OnPlayerEnter_Trigger.cs
+++ Assets/Scripts/OnPlayerEnter_Trigger.cs
@@ -7,15 +7,14 @@ public class OnPlayerEnter_Trigger : MonoBehaviour
 
     public delegate void PlayerEnterTrigger();
     public event PlayerEnterTrigger OnPlayerEnterTrigger;
-
+    bool triggered = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<CharacterController>() != null)
+        if (other.GetComponent<CharacterController>() != null && !triggered)
         {
+            triggered = true;
             OnPlayerEnterTrigger?.Invoke();
-            Collider collider = gameObject.GetComponent<Collider>();
-            Destroy(collider);
         }
     }
 }
